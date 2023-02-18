e7bab86fe84da86f21f716923eea33217c4fb56a
barkha06 <94158669+barkha06@users.noreply.github.com>
2023-02-18 14:49:34 +0530

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << " This is the pipeline";
+  cout <<"TASK - 5";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
